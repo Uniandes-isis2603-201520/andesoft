625e3dcf630459bcc65241251005d1513af3d85f
Juan Sebastian Arciniegas <js.arciniegas10@uniandes.edu.co>
2016-02-18 09:48:06 -0500

    SE ELIMINA MIS VACACIONES

--- Mis Vacaciones/Dev/src/main/webapp/src/modules/login/login.svc.js
+++ Mis Vacaciones/Dev/src/main/webapp/src/modules/login/login.svc.js
@@ -1,65 +1,150 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-(function(ng){
 
-    var mod = ng.module("loginModule");
+  (function () {
+    'use strict';
 
-     mod.service("loginService", ["$http", "loginContext", function ($http, context) {
-        /**
-         * Obtener la lista de Usuarios.
-         * Hace una petición GET con $http a /usuarios para obtener la lista
-         * de usuarios
-         * @returns {promise} promise para leer la respuesta del servidor}
-         * Devuelve una lista de objetos de books con sus atributos y reviews
-         */
-        this.fetchRecords = function () {
-            return $http.get(context);
-        };
+    angular
+        .module('app')
+        .factory('AuthenticationService', AuthenticationService);
 
-        /**
-         * Obtener un registro de usuarios.
-         * Hace una petición GET a /usuario/:id para obtener
-         * los datos de un registro específico de usuario
-         * @param {string} id del registro a obtener
-         * @returns {promise} promise para leer la respuesta del servidor
-         * Devuelve un objeto de books con sus atributos y reviews
-         */
-        this.fetchRecord = function (id) {
-            return $http.get(context + "/" + id);
-        };
+    AuthenticationService.$inject = ['$http', '$cookieStore', '$rootScope', '$timeout', 'UserService'];
+    function AuthenticationService($http, $cookieStore, $rootScope, $timeout, UserService) {
+        var service = {};
 
-        /**
-         * Guardar un registro de usuarios.
-         * Si currentRecord tiene la propiedad id, hace un PUT a /usuarios/:id con los
-         * nuevos datos de la instancia de books.
-         * Si currentRecord no tiene la propiedad id, se hace un POST a /usuarios
-         * para crear el nuevo registro de un usuario
-         * @param {object} currentRecord instancia de usuarios a guardar/actualizar
-         * @returns {promise} promise para leer la respuesta del servidor
-         * Devuelve un objeto de usuario con sus datos incluyendo el id
-         */
-        this.saveRecord = function (currentRecord) {
-            if (currentRecord.id) {
-                return $http.put(context + "/" + currentRecord.id, currentRecord);
-            } else {
-                return $http.post(context, currentRecord);
+        service.Login = Login;
+        service.SetCredentials = SetCredentials;
+        service.ClearCredentials = ClearCredentials;
+
+        return service;
+
+        function Login(username, password, callback) {
+
+            /* Dummy authentication for testing, uses $timeout to simulate api call
+             ----------------------------------------------*/
+            $timeout(function () {
+                var response;
+                UserService.GetByUsername(username)
+                    .then(function (user) {
+                        if (user !== null && user.password === password) {
+                            response = { success: true };
+                        } else {
+                            response = { success: false, message: 'Username or password is incorrect' };
+                        }
+                        callback(response);
+                    });
+            }, 1000);
+
+            /* Use this for real authentication
+             ----------------------------------------------*/
+            //$http.post('/api/authenticate', { username: username, password: password })
+            //    .success(function (response) {
+            //        callback(response);
+            //    });
+
+        }
+
+        function SetCredentials(username, password) {
+            var authdata = Base64.encode(username + ':' + password);
+
+            $rootScope.globals = {
+                currentUser: {
+                    username: username,
+                    authdata: authdata
+                }
+            };
+
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + authdata; // jshint ignore:line
+            $cookieStore.put('globals', $rootScope.globals);
+        }
+
+        function ClearCredentials() {
+            $rootScope.globals = {};
+            $cookieStore.remove('globals');
+            $http.defaults.headers.common.Authorization = 'Basic';
+        }
+    }
+
+    // Base64 encoding service used by AuthenticationService
+    var Base64 = {
+
+        keyStr: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
+
+        encode: function (input) {
+            var output = "";
+            var chr1, chr2, chr3 = "";
+            var enc1, enc2, enc3, enc4 = "";
+            var i = 0;
+
+            do {
+                chr1 = input.charCodeAt(i++);
+                chr2 = input.charCodeAt(i++);
+                chr3 = input.charCodeAt(i++);
+
+                enc1 = chr1 >> 2;
+                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                enc4 = chr3 & 63;
+
+                if (isNaN(chr2)) {
+                    enc3 = enc4 = 64;
+                } else if (isNaN(chr3)) {
+                    enc4 = 64;
+                }
+
+                output = output +
+                    this.keyStr.charAt(enc1) +
+                    this.keyStr.charAt(enc2) +
+                    this.keyStr.charAt(enc3) +
+                    this.keyStr.charAt(enc4);
+                chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+            } while (i < input.length);
+
+            return output;
+        },
+
+        decode: function (input) {
+            var output = "";
+            var chr1, chr2, chr3 = "";
+            var enc1, enc2, enc3, enc4 = "";
+            var i = 0;
+
+            // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+            var base64test = /[^A-Za-z0-9\+\/\=]/g;
+            if (base64test.exec(input)) {
+                window.alert("There were invalid base64 characters in the input text.\n" +
+                    "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+                    "Expect errors in decoding.");
             }
-        };
+            input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
 
-        /**
-         * Hace una petición DELETE a /usuario/:id para eliminar un usuario
-         * @param {string} id identificador de la instancia de book a eliminar
-         * @returns {promise} promise para leer la respuesta del servidor
-         * No devuelve datos.
-         */
-        this.deleteRecord = function (id) {
-            return $http.delete(context + "/" + id);
-        };
-    }]);
+            do {
+                enc1 = this.keyStr.indexOf(input.charAt(i++));
+                enc2 = this.keyStr.indexOf(input.charAt(i++));
+                enc3 = this.keyStr.indexOf(input.charAt(i++));
+                enc4 = this.keyStr.indexOf(input.charAt(i++));
 
+                chr1 = (enc1 << 2) | (enc2 >> 4);
+                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                chr3 = ((enc3 & 3) << 6) | enc4;
 
-})(window.angular);
+                output = output + String.fromCharCode(chr1);
+
+                if (enc3 != 64) {
+                    output = output + String.fromCharCode(chr2);
+                }
+                if (enc4 != 64) {
+                    output = output + String.fromCharCode(chr3);
+                }
+
+                chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+
+            } while (i < input.length);
+
+            return output;
+        }
+    };
+
+})();
+
 
