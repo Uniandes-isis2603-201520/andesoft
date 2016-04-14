ff4a7c5a809db4ac9c90759a82bc46b7b482260b
Juan Sebastian Arciniegas <js.arciniegas10@uniandes.edu.co>
2016-04-14 11:26:27 -0500

    Logic funcionando

---
+++ AndeSoft.POM/AndesSoft.logic/src/main/java/andesoft/api/IUsarioLogic.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package andesoft.api;
+
+import java.util.List;
+import andesoft.entities.UsuarioEntity;
+
+/**
+ *
+ * @author js.arciniegas10
+ */
+public interface IUsarioLogic {
+public UsuarioEntity  getUsuario(Long id);
+    public List<UsuarioEntity> getUsuarios();
+    public UsuarioEntity createUsuario(UsuarioEntity dto);
+    public UsuarioEntity updateUsuario(UsuarioEntity entity);
+    public void deleteUsuario(Long id);
+}
---
+++ AndeSoft.POM/AndesSoft.logic/src/main/java/andesoft/entities/UsuarioEntity.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package andesoft.entities;
+
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author js.arciniegas10
+ */
+public class UsuarioEntity {
+
+  @Id
+    @GeneratedValue(generator = "Usuario")
+    private Long id;
+    private String name;
+    private String apellido;
+    private String usuario;
+    private String correo;
+    private String password;
+//    @ManyToOne(cascade = CascadeType.ALL)
+//    private ItinerarioEntity itinerario;
+//    private List<ItinerarioDTO> itinerarios;
+//    private List<FotoDTO> fotos;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+       public String getNames(){
+        return name;
+    }
+
+    public void setames(String pNomb) {
+        this.name = pNomb;
+    }
+
+       public String getApellidos(){
+        return apellido;
+    }
+
+    public void setApellidos(String pApellidos) {
+        this.apellido = pApellidos;
+    }
+
+    public String getUsuario(){
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+    public void setPassword(String pass) {
+        this.password = pass;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+//    public ItinerarioEntity getItinerario() {
+//        return itinerario;
+//    }
+//
+//    public void setItinerario(ItinerarioEntity itinerario) {
+//        this.itinerario = itinerario;
+//    }
+
+}
+
