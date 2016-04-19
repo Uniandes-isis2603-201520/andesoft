5f80e44a12d2c38b0bc2977e63516bdf00296e6f
afmoreno10 <af.moreno10@uniandes.edu.co>
2016-04-19 05:18:28 -0500

    Modificacion a los elementos logicos de gotel

---
+++ AndeSoft.POM/AndesSoft.logic/src/main/java/andesoft/ejbs/HotelLogic.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package andesoft.ejbs;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import andesoft.api.IHotelLogic;
+import andesoft.persistence.HotelPersistence;
+import andesoft.entities.HotelEntity;
+
+
+@Stateless
+public class HotelLogic implements IHotelLogic
+{
+
+	@Inject
+	private HotelPersistence persistence;
+
+
+
+	@Override
+	public List<HotelEntity> getHoteles()
+	{
+		return persistence.findAll();
+	}
+
+	@Override
+	public HotelEntity getHotel(Long id)
+	{
+		return persistence.find(id);
+	}
+
+	@Override
+	public HotelEntity createHotel(HotelEntity entity)
+	{
+		persistence.create(entity);
+		return entity;
+	}
+
+	@Override
+	public HotelEntity updateHotel (HotelEntity entity)
+	{
+		HotelEntity newEntity = entity;
+		HotelEntity oldEntity = persistence.find(entity.getId());
+
+            //Se deben actualizar las fechas?
+
+		return persistence.update(newEntity);
+	}
+
+	@Override
+	public void deleteHotel(Long id)
+	{
+		persistence.delete(id);
+	}
+
+}
