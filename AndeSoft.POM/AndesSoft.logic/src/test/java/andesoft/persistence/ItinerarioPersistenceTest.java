95bc03eb9c2b435fd31faa0082ebe7d2f8eff5ac
jose gabriel <jg.tamura10@uniandes.edu.co>
2016-04-21 01:12:48 -0500

    Itinerario persistence

---
+++ AndeSoft.POM/AndesSoft.logic/src/test/java/andesoft/persistence/ItinerarioPersistenceTest.java
@@ -0,0 +1,145 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package andesoft.persistence;
+
+import andesoft.entities.ItinerarioEntity;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+@RunWith(Arquillian.class)
+public class ItinerarioPersistenceTest {
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(ItinerarioEntity.class.getPackage())
+                .addPackage(PuntoInteresPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+
+    @Inject
+    private ItinerarioPersistence itinerarioPersistence;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Inject
+    UserTransaction utx;
+
+    private final PodamFactory factory = new PodamFactoryImpl();
+
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    private void clearData() 
+    {
+        em.createQuery("delete from ItinerarioEntity").executeUpdate();
+    }
+
+    private List<ItinerarioEntity> data = new ArrayList<>();
+
+    private void insertData() 
+    {
+        for (int i = 0; i < 3; i++) {
+            ItinerarioEntity entity = factory.manufacturePojo(ItinerarioEntity.class);
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
+
+    @Test
+    public void createItinerarioTest() 
+    {
+        ItinerarioEntity newEntity = factory.manufacturePojo(ItinerarioEntity.class);
+        ItinerarioEntity result = itinerarioPersistence.create(newEntity);
+
+        Assert.assertNotNull(result);
+
+        ItinerarioEntity entity = em.find(ItinerarioEntity.class, result.darId());
+
+        Assert.assertEquals(newEntity.darNombre(), entity.darNombre());
+    }
+
+    @Test
+    public void getItinerariosTest() 
+    {
+        List<ItinerarioEntity> list = itinerarioPersistence.findAll(0);
+        //Assert.assertEquals(data.size(), list.size());
+        for (ItinerarioEntity ent : list) {
+            ItinerarioEntity actual = null;
+            for (ItinerarioEntity entityy : data) {
+                if (ent.darId().equals(entityy.darId())) {
+                    actual = entityy;
+                }
+            }
+            Assert.assertEquals(actual.darIdUsuarioDueño(),0);
+        }
+    }
+
+    @Test
+    public void getItinerarioTest()
+    {
+        ItinerarioEntity entity = data.get(0);
+        ItinerarioEntity newEntity = itinerarioPersistence.find(entity.darId());
+        Assert.assertNotNull(newEntity);
+        Assert.assertEquals(entity.darNombre(), newEntity.darNombre());
+        Assert.assertEquals(entity.darFechaIni(), newEntity.darFechaIni());
+        Assert.assertEquals(entity.darFechaFin(), newEntity.darFechaFin());
+    }
+
+    @Test
+    public void deleteItinerarioTest() 
+    {
+        ItinerarioEntity entity = data.get(0);
+        itinerarioPersistence.delete(entity.darId());
+        ItinerarioEntity deleted = em.find(ItinerarioEntity.class, entity.darId());
+        Assert.assertNull(deleted);
+    }
+
+    @Test
+    public void updateItinerarioTest() {
+        ItinerarioEntity entity = data.get(0);
+        ItinerarioEntity newEntity = factory.manufacturePojo(ItinerarioEntity.class);
+
+        newEntity.setId(entity.darId());
+
+        itinerarioPersistence.update(newEntity);
+
+        ItinerarioEntity resp = em.find(ItinerarioEntity.class, entity.darId());
+
+        Assert.assertEquals(newEntity.darNombre(), resp.darNombre());
+    }
+}
