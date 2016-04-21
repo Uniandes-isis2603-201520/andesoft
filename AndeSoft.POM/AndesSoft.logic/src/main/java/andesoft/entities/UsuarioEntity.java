1482c139c6fd8562955fd01c0b77cb31164a022f
jose gabriel <jg.tamura10@uniandes.edu.co>
2016-04-21 11:17:22 -0500

    Itinerario persistence cambio

--- AndeSoft.POM/AndesSoft.logic/src/main/java/andesoft/entities/UsuarioEntity.java
+++ AndeSoft.POM/AndesSoft.logic/src/main/java/andesoft/entities/UsuarioEntity.java
@@ -10,6 +10,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
@@ -20,8 +21,8 @@ import javax.persistence.ManyToOne;
 @Entity
 public class UsuarioEntity implements Serializable{
 
-  @Id
-    @GeneratedValue(generator = "Usuario")
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String name;
     private String apellido;
