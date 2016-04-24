a6194d54dc318618a0330344872cd6f700fbcfc8
jose gabriel <jg.tamura10@uniandes.edu.co>
2016-04-24 17:03:37 -0500

    arquillian class en itinerario logic test

--- AndeSoft.POM/AndesSoft.logic/src/test/java/andesoft/ejbs/ItinerarioLogicTest.java
+++ AndeSoft.POM/AndesSoft.logic/src/test/java/andesoft/ejbs/ItinerarioLogicTest.java
@@ -15,12 +15,14 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.UserTransaction;
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Before;
+import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
@@ -28,6 +30,7 @@ import uk.co.jemos.podam.api.PodamFactoryImpl;
  *
  * @author jg.tamura10
  */
+@RunWith(Arquillian.class)
 public class ItinerarioLogicTest 
 {
     
