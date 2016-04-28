0210d55b553d2b11d036b7d3be880edf9dc3ea89
afmoreno10 <af.moreno10@uniandes.edu.co>
2016-04-28 14:35:40 -0500

    Pruebas logicas de Hotel funcionales

--- AndeSoft.POM/AndesSoft.logic/src/test/java/andesoft/ejbs/HotelLogicTest.java
+++ AndeSoft.POM/AndesSoft.logic/src/test/java/andesoft/ejbs/HotelLogicTest.java
@@ -1,14 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package andesoft.ejbs;
 
 import andesoft.api.IHotelLogic;
 import andesoft.entities.HotelEntity;
 import andesoft.exceptions.BusinessLogicException;
-import andesoft.persistence.CiudadPersistence;
 import andesoft.persistence.HotelPersistence;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,16 +16,13 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.UserTransaction;
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Assert;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
@@ -34,42 +30,34 @@ import uk.co.jemos.podam.api.PodamFactoryImpl;
  *
  * @author AndresFelipe
  */
-public class HotelLogicTest 
+@RunWith(Arquillian.class)
+public class HotelLogicTest
 {
-      private PodamFactory factory = new PodamFactoryImpl();
-
-      @Inject
-        private IHotelLogic hotelLogic;
-
+    private PodamFactory factory = new PodamFactoryImpl();
+    
+    @Inject
+    private IHotelLogic hotelLogic;
+    
     @PersistenceContext
     private EntityManager em;
-
+    
     @Inject
     private UserTransaction utx;
-
+    
     private List<HotelEntity> data = new ArrayList<HotelEntity>();
-
-   // private List<"otraentity"> otraData = new ArrayList<>();
-
+    
     @Deployment
     public static JavaArchive createDeployment() {
         return ShrinkWrap.create(JavaArchive.class)
                 .addPackage(HotelEntity.class.getPackage())
-                .addPackage(HotelLogic.class.getPackage())
-                .addPackage(IHotelLogic.class.getPackage())
+                .addClass(HotelLogic.class)
+                .addClass(IHotelLogic.class)
                 .addPackage(HotelPersistence.class.getPackage())
+                .addPackage(BusinessLogicException.class.getPackage())
                 .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
                 .addAsManifestResource("META-INF/beans.xml", "beans.xml");
     }
-
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
+    
     @Before
     public void configTest() {
         try {
@@ -86,38 +74,20 @@ public class HotelLogicTest
             }
         }
     }
-
-    @After
-    public void tearDown() throws Exception {
-    }
-
+    
     private void clearData() {
-        em.createQuery("delete from CiudadEntity").executeUpdate();
-        //em.createQuery("delete from otherEntity").executeUpdate();
+        em.createQuery("delete from HotelEntity").executeUpdate();
+        
     }
-
+    
     private void insertData() {
         for (int i = 0; i < 3; i++) {
-            HotelEntity ciudades = factory.manufacturePojo(HotelEntity.class);
-            em.persist(ciudades);
-            data.add(ciudades);
+            HotelEntity hotel = factory.manufacturePojo(HotelEntity.class);
+            em.persist(hotel);
+            data.add(hotel);
         }
-
-        /*
-        for (int i = 0; i < 3; i++) {
-            OtherEntity entity = factory.manufacturePojo(OtherEntity.class);
-
-            em.persist(entity);
-            data.add(entity);
-
-            otherData.add(entity)
-        }
-        */
     }
 
-    /**
-     *
-     */
     @Test
     public void createHotelTest() {
         HotelEntity entity = factory.manufacturePojo(HotelEntity.class);
@@ -127,11 +97,11 @@ public class HotelLogicTest
         Assert.assertEquals(entity.getid(), result.getid());
         Assert.assertEquals(entity.getName(), result.getName());
     }
-
+    
     @Test
     public void getHotelesTest() {
         List<HotelEntity> resultList = hotelLogic.getHoteles();
-        List<HotelEntity> expectedList = em.createQuery("SELECT u from CiudadEntity u").getResultList();
+        List<HotelEntity> expectedList = em.createQuery("SELECT u from HotelEntity u").getResultList();
         Assert.assertEquals(expectedList.size(), resultList.size());
         for (HotelEntity expected : expectedList) {
             boolean found = false;
@@ -143,26 +113,26 @@ public class HotelLogicTest
             Assert.assertTrue(found);
         }
     }
-
+    
     @Test
     public void getHotelTest() throws BusinessLogicException {
         HotelEntity result = hotelLogic.getHotel((data.get(0).getid()));
         HotelEntity expected = em.find(HotelEntity.class, data.get(0).getid());
-
+        
         Assert.assertNotNull(expected);
         Assert.assertNotNull(result);
         Assert.assertEquals(expected.getid(), result.getid());
         Assert.assertEquals(expected.getName(), result.getName());
     }
-
-     @Test
+    
+    @Test
     public void deleteHotelTest() {
         HotelEntity entity = data.get(1);
         hotelLogic.deleteHotel(entity.getid());
         HotelEntity deleted = em.find(HotelEntity.class, entity.getid());
         Assert.assertNull(deleted);
     }
-
+    
     @Test
     public void updateHotelTest() {
         HotelEntity entity = data.get(0);
@@ -172,79 +142,5 @@ public class HotelLogicTest
         HotelEntity resp = em.find(HotelEntity.class, entity.getid());
         Assert.assertEquals(pojoEntity.getid(), resp.getid());
         Assert.assertEquals(pojoEntity.getName(), resp.getName());
-    }
-
-    /**
-     * Test of getHoteles method, of class HotelLogic.
-     */
-    @Test
-    public void testGetHoteles() throws Exception {
-        System.out.println("getHoteles");
-        HotelLogic instance = new HotelLogic();
-        List<HotelEntity> expResult = null;
-        List<HotelEntity> result = instance.getHoteles();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getHotel method, of class HotelLogic.
-     */
-    @Test
-    public void testGetHotel() throws Exception {
-        System.out.println("getHotel");
-        Long id = null;
-        HotelLogic instance = new HotelLogic();
-        HotelEntity expResult = null;
-        HotelEntity result = instance.getHotel(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of createHotel method, of class HotelLogic.
-     */
-    @Test
-    public void testCreateHotel() throws Exception {
-        System.out.println("createHotel");
-        HotelEntity entity = null;
-        HotelLogic instance = new HotelLogic();
-        HotelEntity expResult = null;
-        HotelEntity result = instance.createHotel(entity);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of updateHotel method, of class HotelLogic.
-     */
-    @Test
-    public void testUpdateHotel() throws Exception {
-        System.out.println("updateHotel");
-        HotelEntity entity = null;
-        HotelLogic instance = new HotelLogic();
-        HotelEntity expResult = null;
-        HotelEntity result = instance.updateHotel(entity);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of deleteHotel method, of class HotelLogic.
-     */
-    @Test
-    public void testDeleteHotel() throws Exception {
-        System.out.println("deleteHotel");
-        Long id = null;
-        HotelLogic instance = new HotelLogic();
-        instance.deleteHotel(id);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-
+    }    
 }
